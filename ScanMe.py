616841b2e779fa8993ecbaa5664dd4db6c42390e
Jules-gatete <j.gatete@alustudent.com>
2025-02-12 01:12:10 +0200

    Add QR code generation functionality in ScanMe.py and update .gitignore

--- ScanMe.py
+++ ScanMe.py
@@ -1,14 +1,23 @@
 import qrcode
 
+# Data to be encoded
+data = ""
+
+# Create QR Code
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-
-qr.add_data('https://jules-gatete-portfolio.vercel.app/')
+qr.add_data(data)
 qr.make(fit=True)
 
+# Create an image from the QR Code instance
 img = qr.make_image(fill='black', back_color='white')
-img.save('qrcode.png')
+
+# Save the image
+img.save("qrcode.png")
+
+# Optionally, display the image
+img.show()
